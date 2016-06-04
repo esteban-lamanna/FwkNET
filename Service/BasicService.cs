487d52f51f962658fdc009ce99f64762350d9dfa
esteban210891 <estebanlamanna@hotmail.com>
2016-06-03 23:20:22 -0300

    eliminado proy MVC

--- Service/BasicService.cs
+++ Service/BasicService.cs
@@ -8,14 +8,14 @@ using ViewModelEntities;
 
 namespace Service
 {
-    public abstract class EntityManagerUIService<TEntity, DVM, EV, EVM, C, FVM, TBusinessService> : IEntityManagerBusinessService<DVM, EVM, FVM>
+    public abstract class EntityManagerUIService/*<TEntity, DVM, EV, EVM, C, FVM, TBusinessService> : IEntityManagerBusinessService<DVM, EVM, FVM>
         where TEntity : BusinessEntity, new()
         where DVM : DetailModel, new()
         where EV : EntityExtendedView
         where EVM : ResultModel, new()
         where C : CriteriaEntity, new()
         where FVM : FilterModel
-        where TBusinessService : IEntityManagerBusinessService<TEntity, EV, C>
+        where TBusinessService : IEntityManagerBusinessService<TEntity, EV, C>*/
     {
     } 
     
